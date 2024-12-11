0bfd9c5918f087c6aba8f5a1d7832b84a38cd1a1
xianzicai <xianzicai@qq.com>
2024-12-11 18:18:07 +0800

    初始项目

--- env.js
+++ env.js
@@ -2,7 +2,7 @@
 module.exports = {
     dev: {
         DESCN:'开发环境',
-        API_BASE_URL:'http://159.75.109.161:8160',
+        API_BASE_URL:'http://localhost:8160',
     },
     prod: {
         DESCN:'线上环境',
